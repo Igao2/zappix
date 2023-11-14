e7e223604b908f6c551840ab5e1d645fe66a7980
Igao2 <igor.dasilvacosta8@gmail.com>
2023-11-14 08:36:05 -0300

    frontend improves

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var button = document.getElementById("enviar")
+var button = document.getElementById("enviarZap")
 button.addEventListener("click",function(){
 
     var telefone = document.getElementById("telefone").value
     var mensagem = document.getElementById("mensagem").value
+
+    if(mensagem!="")
+    {
     const whatsappLink = `https://api.whatsapp.com/send?phone=${telefone}&text=${encodeURIComponent(mensagem)}`;
     var link = document.getElementById("resultado")
     link.innerHTML= whatsappLink
+    }
+    else{
+    const whatsappLink = `https://api.whatsapp.com/send?phone=${telefone}`
+    var link = document.getElementById("resultado")
+    link.innerHTML= whatsappLink
+    }
+    
 })
